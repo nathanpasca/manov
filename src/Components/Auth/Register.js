347bfc348a7216e21da96c7e52af32b2faff7a44
nathanpasca <nathandwi13@gmail.com>
2024-04-24 01:49:57 +0700

    added formik, removed firebase

--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -1,6 +1,14 @@
 import React from "react"
+import { useFormik } from "formik"
 
 function Register() {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  })
   return (
     <div>
       <h1>Register PAGE</h1>
