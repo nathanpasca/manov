028b0daaf8f4637688ca6eded2c7bc22104ebfc4
nathanpasca <nathandwi13@gmail.com>
2024-06-02 02:40:57 +0700

    Implement Create Novel

--- src/Components/Novel/CreateNovel.js
+++ src/Components/Novel/CreateNovel.js
@@ -1,9 +1,26 @@
-import React, { Component } from "react"
+import React, { Component, useEffect, useState } from "react"
+import ReactQuill from "react-quill"
+import "react-quill/dist/quill.snow.css"
 
-class CreateNovel extends Component {
-  render() {
-    return <div></div>
-  }
+function CreateNovel() {
+  const [content, setContent] = useState("")
+
+  return (
+    <section>
+      <div className='flex justify-center md:mx-24 sm:mx-8 flex-col'>
+        <h1 className='text-4xl'>Create Your Own Novel!</h1>
+        <div className='w-full max-h-min mt-24'>
+          <label class='form-control w-full max-w-lg'>
+            <div class='label'>
+              <span class='label-text'>Judul:</span>
+            </div>
+            <input type='text' placeholder='Type here' class='input input-bordered w-full max-w-lg' />
+          </label>
+          <ReactQuill value={content} onChange={setContent} className='w-[50%]' />
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default CreateNovel
