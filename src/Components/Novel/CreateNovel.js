cfe619944cfcb39d624036c1b90e6d3eee8a37e5
nathanpasca <nathandwi13@gmail.com>
2024-11-28 21:46:27 +0700

    test

--- src/Components/Novel/CreateNovel.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react"
-
-class CreateNovel extends Component {
-  render() {
-    return <div></div>
-  }
-}
-
-export default CreateNovel
--- src/Pages/RegisterPage.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import Register from "../Components/Auth/Register"
-
-export default function RegisterPage() {
-  return <Register />
-}
